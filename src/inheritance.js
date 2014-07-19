957138cedfd50316caf9ad7e46cf3bb2f736b4cc
srdjan <srdjan@timok.com>
2014-07-19 18:06:30 -0400

    cleanup

---
+++ src/inheritance.js
@@ -0,0 +1,45 @@
+//- person
+var Person = function() {
+    this.canTalk = true;
+    this.greet = function() {
+        if (this.canTalk) {
+            console.log("Hi, I'm " + this.name);
+        }
+    };
+};
+
+//- employee
+var Employee = function(name, title) {
+    this.name = name;
+    this.title = title;
+    this.greet = function() {
+        if (this.canTalk) {
+            console.log("Hi, I'm " + this.name + ", the " + this.title);
+        }
+    };
+};
+Employee.prototype = new Person();
+
+//- customer
+var Customer = function(name) {
+    this.name = name;
+};
+Customer.prototype = new Person();
+
+//-- mime
+var Mime = function(name) {
+    this.name = name;
+    this.canTalk = false;
+};
+Mime.prototype = new Person();
+
+var bob = new Employee('Bob','Builder');
+var joe = new Customer('Joe');
+var rg = new Employee('Red Green','Handyman');
+var mike = new Customer('Mike');
+var mime = new Mime('Mime');
+bob.greet();
+joe.greet();
+rg.greet();
+mike.greet();
+mime.greet();
