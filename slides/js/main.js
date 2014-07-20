63c290078a35ebf73091fbace777963dee8f390e
srdjan <srdjan@timok.com>
2014-07-20 13:46:31 -0400

    added Why Functional section

--- slides/js/main.js
+++ slides/js/main.js
@@ -1,13 +1,29 @@
 $(function() {
-	// Deck initialization
-	$.deck('.slide');
-	
-	$('#style-themes').change(function() {
-		$('#style-theme-link').attr('href', $(this).val());
-	});
-	
-	$('#transition-themes').change(function() {
-		$('#transition-theme-link').attr('href', $(this).val());
-	});
+    // Deck initialization
+    $.deck('.slide');
+
+    $('#style-themes').change(function() {
+        $('#style-theme-link').attr('href', $(this).val());
+    });
+
+    $('#transition-themes').change(function() {
+        $('#transition-theme-link').attr('href', $(this).val());
+    });
+		
+		show();
 });
 
+function show() {
+    var clock = new Date();
+    var hours = clock.getHours();
+    var minutes = clock.getMinutes();
+    var seconds = clock.getSeconds();
+    if (minutes <= 9) {
+        minutes = "0" + minutes;
+    }
+    if (seconds <= 9) {
+        seconds = "0" + seconds;
+    }
+    document.Tick.Clock.value = hours + ":" + minutes + ":" + seconds + ' ';
+    setTimeout("show()", 1000);
+}
