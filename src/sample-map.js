ae525d984f0fb8831961372889fa8377185a9c30
srdjan <srdjan@timok.com>
2014-07-28 12:54:42 -0400

    upfated ppt/pdf deck

--- src/sample-map.js
+++ src/sample-map.js
@@ -2,6 +2,8 @@ require('../node_modules/functionaljs').expose(global);
 
 var show = console.log;
 
-show(map(function (n) { 
+function sqr(n) { 
   return n*n; 
-}, [1, 2, 3, 4, 5]));
+};
+
+show(map(sqr, [1, 2, 3, 4, 5]));
