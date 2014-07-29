74d102650205624b50f02e7b31b8781aff84ebf4
srdjan <srdjan@timok.com>
2014-07-29 11:15:02 -0400

    fixed last 'map' sample

--- src/sample-map.js
+++ src/sample-map.js
@@ -1,9 +1,17 @@
 require('../node_modules/functionaljs').expose(global);
-
 var show = console.log;
 
 function sqr(n) { 
   return n*n; 
 };
 
-show(map(sqr, [1, 2, 3, 4, 5]));
+// show(map(sqr, [1, 2, 3, 4, 5]));
+
+function splat(fn) {
+  return function (list) {
+    return Array.prototype.map.call(list, fn);
+  }
+}
+
+var sqrMap = splat(sqr);
+show(sqrMap([1, 2, 3, 4, 5]));
